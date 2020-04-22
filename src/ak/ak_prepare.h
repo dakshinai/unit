ea119e9ce5a698d66f63fd643b6fb73ae5651e03
dakshinai <akdaks08@gmail.com>
2020-04-21 18:36:27 -0700

    AK - AF_XDP umem server

---
+++ src/ak/ak_prepare.h
@@ -0,0 +1,541 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright(c) 2017 - 2018 Intel Corporation. */
+
+//#include <asm/barrier.h>
+#include <errno.h>
+#include <getopt.h>
+#include <libgen.h>
+#include <linux/bpf.h>
+//#include <linux/compiler.h>
+#include <linux/if_link.h>
+#include <linux/if_xdp.h>
+#include <linux/if_ether.h>
+#include <locale.h>
+#include <net/ethernet.h>
+#include <net/if.h>
+#include <poll.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <sys/un.h>
+
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+#include <bpf/libbpf.h>
+#include <bpf/xsk.h>
+#include <bpf/bpf.h>
+
+#ifndef SOL_XDP
+#define SOL_XDP 283
+#endif
+
+#ifndef AF_XDP
+#define AF_XDP 44
+#endif
+
+#ifndef PF_XDP
+#define PF_XDP AF_XDP
+#endif
+
+#define MAX_SOCKS 4
+
+#define NUM_FRAMES (4 * 1024)
+#define BATCH_SIZE 64
+
+#define DEBUG_HEXDUMP 0
+
+typedef __u64 u64;
+typedef __u32 u32;
+
+enum benchmark_type
+{
+    BENCH_RXDROP = 0,
+    BENCH_TXONLY = 1,
+    BENCH_L2FWD = 2,
+};
+
+static enum benchmark_type opt_bench = BENCH_RXDROP;
+static u32 opt_xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST;
+static const char *opt_if = "";
+static int opt_ifindex;
+static int opt_queue;
+static int opt_poll;
+static int opt_interval = 1;
+static u32 opt_xdp_bind_flags = XDP_USE_NEED_WAKEUP;
+static u32 opt_umem_flags;
+static int opt_unaligned_chunks;
+static int opt_mmap_flags;
+static u32 opt_xdp_bind_flags;
+static int opt_xsk_frame_size = XSK_UMEM__DEFAULT_FRAME_SIZE;
+static bool opt_need_wakeup = true;
+static u32 opt_num_xsks = 1;
+static u32 prog_id;
+
+struct xsk_umem_info_shared
+{
+    struct xsk_ring_prod fq;
+    struct xsk_ring_cons cq;
+    struct xsk_umem umem;
+    void *buffer; //refactor to remove this
+};
+
+//TODO: This will later change based on the number of dependent applications
+const char *AK_share_name = "AK_shared_memory";
+const char *umem_area_share_name = "AK_umem_shared_memory";
+const int shm_size = 4096 * 4096;
+int ak_shm_fd;
+int umem_shm_fd;
+struct xsk_umem_info_shared **umem_ptr = NULL;
+
+static void remove_xdp_program(void)
+{
+    u32 curr_prog_id = 0;
+
+    if (bpf_get_link_xdp_id(opt_ifindex, &curr_prog_id, opt_xdp_flags))
+    {
+        fprintf(stderr, "bpf_get_link_xdp_id failed\n");
+        exit(EXIT_FAILURE);
+    }
+    if (prog_id == curr_prog_id)
+        bpf_set_link_xdp_fd(opt_ifindex, -1, opt_xdp_flags);
+    else if (!curr_prog_id)
+        fprintf(stderr, "couldn't find a prog id on a given interface\n");
+    else
+        fprintf(stderr, "program on interface changed, not removing\n");
+}
+
+static void int_exit_shared(int sig)
+{
+
+    struct xsk_umem_info_shared *umem = *umem_ptr;
+    (void)xsk_umem__delete(&umem->umem);
+    remove_xdp_program();
+    shm_unlink(AK_share_name);
+    shm_unlink(umem_area_share_name);
+
+    exit(EXIT_SUCCESS);
+}
+
+/*
+static void __exit_with_error(int error, const char *file, const char *func,
+                              int line)
+{
+    fprintf(stderr, "%s:%s:%i: errno: %d/\"%s\"\n", file, func, line, error,
+            strerror(error));
+    remove_xdp_program();
+    exit(EXIT_FAILURE);
+}
+
+#define exit_with_error(error) \
+    __exit_with_error(error, __FILE__, __func__, __LINE__)
+*/
+
+static void __print_with_error(int error, const char *file, const char *func,
+                               int line)
+{
+    fprintf(stderr, "%s:%s:%i: errno: %d/\"%s\"\n", file, func, line, error,
+            strerror(error));
+}
+
+#define print_with_error(error) \
+    __print_with_error(error, __FILE__, __func__, __LINE__)
+
+static int load_xdp_program(char **argv, struct bpf_object **obj)
+{
+    struct bpf_prog_load_attr prog_load_attr = {
+        .prog_type = BPF_PROG_TYPE_XDP,
+    };
+    char xdp_filename[256];
+    int prog_fd;
+
+    snprintf(xdp_filename, sizeof(xdp_filename), "%s_kern.o", argv[0]);
+    prog_load_attr.file = xdp_filename;
+
+    if (bpf_prog_load_xattr(&prog_load_attr, obj, &prog_fd))
+    {
+        return -1;
+    }
+
+    if (prog_fd < 0)
+    {
+        fprintf(stderr, "ERROR: no program found: %s\n",
+                strerror(prog_fd));
+        return -1;
+    }
+
+    if (bpf_set_link_xdp_fd(opt_ifindex, prog_fd, opt_xdp_flags) < 0)
+    {
+        fprintf(stderr, "ERROR: link set xdp fd failed\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static struct option long_options[] = {
+    {"rxdrop", no_argument, 0, 'r'},
+    {"txonly", no_argument, 0, 't'},
+    {"l2fwd", no_argument, 0, 'l'},
+    {"interface", required_argument, 0, 'i'},
+    {"queue", required_argument, 0, 'q'},
+    {"poll", no_argument, 0, 'p'},
+    {"xdp-skb", no_argument, 0, 'S'},
+    {"xdp-native", no_argument, 0, 'N'},
+    {"interval", required_argument, 0, 'n'},
+    {"zero-copy", no_argument, 0, 'z'},
+    {"copy", no_argument, 0, 'c'},
+    {"frame-size", required_argument, 0, 'f'},
+    {"no-need-wakeup", no_argument, 0, 'm'},
+    {"unaligned", no_argument, 0, 'u'},
+    {"shared-umem", no_argument, 0, 'M'},
+    {"force", no_argument, 0, 'F'},
+    {0, 0, 0, 0}};
+
+static void usage(const char *prog)
+{
+    const char *str =
+        "  Usage: %s [OPTIONS]\n"
+        "  Options:\n"
+        "  -r, --rxdrop		Discard all incoming packets (default)\n"
+        "  -t, --txonly		Only send packets\n"
+        "  -l, --l2fwd		MAC swap L2 forwarding\n"
+        "  -i, --interface=n	Run on interface n\n"
+        "  -q, --queue=n	Use queue n (default 0)\n"
+        "  -p, --poll		Use poll syscall\n"
+        "  -S, --xdp-skb=n	Use XDP skb-mod\n"
+        "  -N, --xdp-native=n	Enforce XDP native mode\n"
+        "  -n, --interval=n	Specify statistics update interval (default 1 sec).\n"
+        "  -z, --zero-copy      Force zero-copy mode.\n"
+        "  -c, --copy           Force copy mode.\n"
+        "  -m, --no-need-wakeup Turn off use of driver need wakeup flag.\n"
+        "  -f, --frame-size=n   Set the frame size (must be a power of two in aligned mode, default is %d).\n"
+        "  -u, --unaligned	Enable unaligned chunk placement\n"
+        "  -M, --shared-umem	Enable XDP_SHARED_UMEM\n"
+        "  -F, --force		Force loading the XDP prog\n"
+        "\n";
+    fprintf(stderr, str, prog, XSK_UMEM__DEFAULT_FRAME_SIZE);
+    //exit(EXIT_FAILURE);
+}
+
+static void parse_command_line(int argc, char **argv)
+{
+    int option_index, c;
+
+    opterr = 0;
+
+    for (;;)
+    {
+        c = getopt_long(argc, argv, "Frtli:q:psSNn:czf:muM",
+                        long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 'r':
+            opt_bench = BENCH_RXDROP;
+            break;
+        case 't':
+            opt_bench = BENCH_TXONLY;
+            break;
+        case 'l':
+            opt_bench = BENCH_L2FWD;
+            break;
+        case 'i':
+            opt_if = optarg;
+            break;
+        case 'q':
+            opt_queue = atoi(optarg);
+            break;
+        case 'p':
+            opt_poll = 1;
+            break;
+        case 'S':
+            opt_xdp_flags |= XDP_FLAGS_SKB_MODE;
+            opt_xdp_bind_flags |= XDP_COPY;
+            break;
+        case 'N':
+            opt_xdp_flags |= XDP_FLAGS_DRV_MODE;
+            break;
+        case 'n':
+            opt_interval = atoi(optarg);
+            break;
+        case 'z':
+            opt_xdp_bind_flags |= XDP_ZEROCOPY;
+            break;
+        case 'c':
+            opt_xdp_bind_flags |= XDP_COPY;
+            break;
+        case 'u':
+            opt_umem_flags |= XDP_UMEM_UNALIGNED_CHUNK_FLAG;
+            opt_unaligned_chunks = 1;
+            opt_mmap_flags = MAP_HUGETLB;
+            break;
+        case 'F':
+            opt_xdp_flags &= ~XDP_FLAGS_UPDATE_IF_NOEXIST;
+            break;
+        case 'f':
+            opt_xsk_frame_size = atoi(optarg);
+            break;
+        case 'm':
+            opt_need_wakeup = false;
+            opt_xdp_bind_flags &= ~XDP_USE_NEED_WAKEUP;
+            break;
+        case 'M':
+            opt_num_xsks = MAX_SOCKS;
+            break;
+        default:
+            usage(basename(argv[0]));
+        }
+    }
+
+    opt_ifindex = if_nametoindex(opt_if);
+    if (!opt_ifindex)
+    {
+        fprintf(stderr, "ERROR: interface \"%s\" does not exist\n",
+                opt_if);
+        usage(basename(argv[0]));
+    }
+
+    if ((opt_xsk_frame_size & (opt_xsk_frame_size - 1)) &&
+        !opt_unaligned_chunks)
+    {
+        fprintf(stderr, "--frame-size=%d is not a power of two\n",
+                opt_xsk_frame_size);
+        usage(basename(argv[0]));
+    }
+}
+
+static struct xsk_umem_info_shared *
+xsk_configure_umem_shared(int shm_fd, int umem_shm_fd, int umem_size,
+                          int umem_mmap_flags)
+{
+    struct xsk_umem_config cfg = {
+        .fill_size = XSK_RING_PROD__DEFAULT_NUM_DESCS,
+        .comp_size = XSK_RING_CONS__DEFAULT_NUM_DESCS,
+        .frame_size = opt_xsk_frame_size,
+        .frame_headroom = XSK_UMEM__DEFAULT_FRAME_HEADROOM,
+        .flags = opt_umem_flags};
+
+    struct xsk_umem_info_shared *umem = mmap(NULL, sizeof(struct xsk_umem_info_shared),
+                                             PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    if (umem == MAP_FAILED)
+    {
+        fprintf(stderr, "ERROR: umem creation failed\n");
+        print_with_error(-errno);
+    }
+
+    // Reserve memory for the umem. Use hugepages if unaligned chunk mode
+    umem->buffer = mmap(NULL, umem_size, PROT_READ | PROT_WRITE,
+                        MAP_SHARED | MAP_ANONYMOUS | umem_mmap_flags,
+                        umem_shm_fd, 0);
+    if (umem->buffer == MAP_FAILED)
+    {
+        fprintf(stderr, "ERROR: umem buffer creation failed\n");
+        print_with_error(-errno);
+    }
+
+    int ret = xsk_umem__create_shared(&umem->umem, umem->buffer, umem_size,
+                                      &umem->fq, &umem->cq, &cfg, shm_fd);
+
+    if (ret < 0)
+    {
+        fprintf(stderr, "ERROR: umem setup failed\n");
+        print_with_error(ret);
+    }
+
+    return umem;
+}
+
+static int xsk_populate_fill_ring_shared(struct xsk_umem_info_shared *umem)
+{
+    int ret, i;
+    u32 idx;
+
+    ret = xsk_ring_prod__reserve(&umem->fq,
+                                 XSK_RING_PROD__DEFAULT_NUM_DESCS, &idx);
+    if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS)
+    {
+        return -1;
+    }
+    for (i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i++)
+        *xsk_ring_prod__fill_addr(&umem->fq, idx++) =
+            i * opt_xsk_frame_size;
+    xsk_ring_prod__submit(&umem->fq, XSK_RING_PROD__DEFAULT_NUM_DESCS);
+
+    return 0;
+}
+
+static int AK_send_fds(int socket, int *fds, int n)
+{
+    struct msghdr message;
+    struct iovec iov[1];
+    struct cmsghdr *control_message = NULL;
+    char ctrl_buf[CMSG_SPACE(sizeof(int) * n)];
+    char data[1];
+
+    memset(&message, 0, sizeof(struct msghdr));
+    memset(ctrl_buf, 0, CMSG_SPACE(sizeof(int) * n));
+
+    // We are passing at least one byte of data so that recvmsg() will not return 0
+    data[0] = ' ';
+    iov[0].iov_base = data;
+    iov[0].iov_len = sizeof(data);
+
+    message.msg_name = NULL;
+    message.msg_namelen = 0;
+    message.msg_iov = iov;
+    message.msg_iovlen = 1;
+    message.msg_controllen = CMSG_SPACE(sizeof(int) * n);
+    message.msg_control = ctrl_buf;
+
+    control_message = CMSG_FIRSTHDR(&message);
+    control_message->cmsg_level = SOL_SOCKET;
+    control_message->cmsg_type = SCM_RIGHTS;
+    control_message->cmsg_len = CMSG_LEN(sizeof(int) * n);
+
+    memcpy((int *)CMSG_DATA(control_message), fds, n * sizeof(int));
+
+    return sendmsg(socket, &message, 0);
+}
+
+/*static int AK_create_shared_memory(const char *name, int size)
+{
+    // create the shared memory object 
+    int shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    if (shm_fd < 0)
+        return shm_fd;
+    // configure the size of the shared memory object 
+    ftruncate(shm_fd, size);
+    return shm_fd;
+}*/
+
+static int AK_create_shared_memory_regions()
+{
+    int ret;
+
+    ak_shm_fd = shm_open(AK_share_name, O_CREAT | O_RDWR, 0666);
+    if (ak_shm_fd < 0)
+    {
+        fprintf(stderr, "ERROR: unable to create AK shared memory\n");
+        return -1;
+    }
+
+    ret = ftruncate(ak_shm_fd, shm_size);
+    if (ret < 0)
+    {
+        fprintf(stderr, "ERROR: unable to set AK shared memory size\n");
+        print_with_error(-errno);
+        return -errno;
+    }
+
+    umem_shm_fd = shm_open(umem_area_share_name, O_CREAT | O_RDWR, 0666);
+    if (umem_shm_fd < 0)
+    {
+        fprintf(stderr, "ERROR: unable to create AK umem shared memory\n");
+        return -1;
+    }
+
+    ret = ftruncate(umem_shm_fd, shm_size);
+    if (ret < 0)
+    {
+        fprintf(stderr, "ERROR: unable to set AK umem shared memory size\n");
+        print_with_error(-errno);
+        return -errno;
+    }
+
+    return 0;
+}
+static struct xsk_umem_info_shared *AK_create_umem_shared_memory()
+{
+    return xsk_configure_umem_shared(ak_shm_fd, umem_shm_fd,
+                                     NUM_FRAMES * opt_xsk_frame_size,
+                                     opt_mmap_flags);
+}
+
+static int AK_socket_send_umem_fd(int fd)
+{
+    struct sockaddr_un addr;
+    int sock;
+    int conn;
+    int ret;
+
+    //TODO: This will later change based on the number of dependent applications
+    const char *AK_socket_path = "/AK_socket";
+
+    int fds[1];
+    fds[0] = fd;
+
+    // Create a unix domain socket
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        print_with_error(-errno);
+        return -errno;
+    }
+    // Bind it to a abstract address
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strcpy(&addr.sun_path[1], AK_socket_path);
+
+    ret = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret < 0)
+    {
+        print_with_error(-errno);
+        return -errno;
+    }
+
+    fprintf(stdout, "umem fd waiting to be sent=%d\n", fds[0]);
+
+    // Listen
+    ret = listen(sock, 1);
+    if (ret < 0)
+    {
+        print_with_error(-errno);
+        return -errno;
+    }
+
+    // Just send the file descriptor to anyone who connects
+    //TODO: Later co-relate requests
+    for (;;)
+    {
+        conn = accept(sock, NULL, 0);
+        if (conn < 0)
+        {
+            print_with_error(-errno);
+            return -errno;
+        }
+
+        ret = AK_send_fds(conn, fds, 1);
+        if (ret < 0)
+        {
+            print_with_error(-errno);
+            return -errno;
+        }
+
+        fprintf(stdout, "Sent umem fd=%d\n", fds[0]);
+
+        ret = close(conn);
+        if (ret < 0)
+        {
+            print_with_error(-errno);
+            return -errno;
+        }
+        //close(sock);
+    }
+
+    return 0;
+}
--- src/nxt_application_kernel.c
+++ src/nxt_application_kernel.c
@@ -3,38 +3,37 @@
 #include <nxt_conf.h>
 #include <nxt_port_memory_int.h>
 #include <nxt_http.h>
+#include <ak/ak_prepare.h>
 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8087
 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8087 
+static nxt_application_kernel_t *nxt_application_kernel;
 
-
-static nxt_application_kernel_t  *nxt_application_kernel;
-
-static void nxt_application_kernel_greet_controller(nxt_task_t *task,
-    nxt_port_t *controller_port);
+static nxt_int_t nxt_application_kernel_greet_controller(nxt_task_t *task,
+                                                         nxt_port_t *controller_port);
 
 static void nxt_application_kernel_oosm_handler(nxt_task_t *task, nxt_port_recv_msg_t *msg);
 
-nxt_port_handlers_t  nxt_application_kernel_process_port_handlers = {
-    .quit         = nxt_worker_process_quit_handler,
-    .new_port     = nxt_application_kernel_new_port_handler,
-    .change_file  = nxt_port_change_log_file_handler,
-    .mmap         = nxt_port_mmap_handler,
+nxt_port_handlers_t nxt_application_kernel_process_port_handlers = {
+    .quit = nxt_worker_process_quit_handler,
+    .new_port = nxt_application_kernel_new_port_handler,
+    .change_file = nxt_port_change_log_file_handler,
+    .mmap = nxt_port_mmap_handler,
     //.data         = nxt_router_conf_data_handler,
     //.remove_pid   = nxt_router_remove_pid_handler,
     //.access_log   = nxt_router_access_log_reopen_handler,
-    .rpc_ready    = nxt_port_rpc_handler,
-    .rpc_error    = nxt_port_rpc_handler,
-    .oosm         = nxt_application_kernel_oosm_handler,
+    .rpc_ready = nxt_port_rpc_handler,
+    .rpc_error = nxt_port_rpc_handler,
+    .oosm = nxt_application_kernel_oosm_handler,
 };
 
-
+/*
 static void run_socket_file_server(nxt_task_t *task){
 
     //nxt_log(task, NXT_LOG_INFO, "APPLICATION_KERNEL: run_socket_file_server"); 
@@ -104,33 +103,136 @@ static void run_socket_file_server(nxt_task_t *task){
     }
     
 }
+*/
 
-static void
-nxt_application_kernel_greet_controller(nxt_task_t *task, nxt_port_t *controller_port)
+static nxt_int_t run_AF_XDP_umem_server(nxt_task_t *task)
 {
-    nxt_log(task, NXT_LOG_INFO, "Hello from application_kernel");
-    run_socket_file_server(task);
-    nxt_port_socket_write(task, controller_port, NXT_PORT_MSG_PROCESS_READY,
-                          -1, 0, 0, NULL);
+    //TODO: Check access
+    nxt_log(task, NXT_LOG_INFO, "APPLICATION_KERNEL: prepping the af_xdp umem env");
+
+    struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+    struct bpf_object *obj;
+    int ret;
+    struct xsk_umem_info_shared *umem;
+
+    //TODO: Hardcoded, this will change when AK provides an user interface
+    int argc = 9;
+    char *argv[] = {strdup("nxt_application_kernel"), strdup("-i"), strdup("eth0"), strdup("-q"), strdup("28"), strdup("-n"), strdup("3"), strdup("-r"), strdup("-N")};
+
+    parse_command_line(argc, argv);
+
+    opt_bench = BENCH_RXDROP;
+
+    if (setrlimit(RLIMIT_MEMLOCK, &r))
+    {
+        nxt_log(task, NXT_LOG_ERR, "APPLICATION_KERNEL: setrlimit(RLIMIT_MEMLOCK) %s", strerror(errno));
+        return NXT_ERROR;
+    }
+
+    if (opt_num_xsks > 1)
+    {
+        ret = load_xdp_program(argv, &obj);
+        if (ret < 0)
+        {
+            nxt_log(task, NXT_LOG_ERR, "APPLICATION_KERNEL: load xdp program failed");
+            return NXT_ERROR;
+        }
+    }
+
+    ret = AK_create_shared_memory_regions();
+    if (ret < 0)
+    {
+        nxt_log(task, NXT_LOG_ERR, "APPLICATION_KERNEL: unable to create shared memory regions");
+        return NXT_ERROR;
+    }
+
+    umem = AK_create_umem_shared_memory();
+
+    if (umem == NULL)
+    {
+        nxt_log(task, NXT_LOG_ERR, "APPLICATION_KERNEL: umem is NULL");
+        return NXT_ERROR;
+    }
+
+    umem_ptr = &umem;
+
+    nxt_log(task, NXT_LOG_INFO, "APPLICATION_KERNEL: umem configured");
+
+    if (opt_bench == BENCH_RXDROP || opt_bench == BENCH_L2FWD)
+    {
+        ret = xsk_populate_fill_ring_shared(umem);
+        if (ret < 0)
+        {
+            nxt_log(task, NXT_LOG_ERR, "APPLICATION_KERNEL: populate umem fill rings failed");
+            return NXT_ERROR;
+        }
+
+        nxt_log(task, NXT_LOG_INFO, "APPLICATION_KERNEL: umem fill rings configured");
+    }
+
+    nxt_log(task, NXT_LOG_INFO, "APPLICATION_KERNEL: waiting to distribute umem fd %d", xsk_umem__fd(&umem->umem));
+
+    ret = AK_socket_send_umem_fd(xsk_umem__fd(&umem->umem));
+
+    if (ret < 0)
+    {
+        nxt_log(task, NXT_LOG_ERR, "APPLICATION_KERNEL: unable to send umem socket descriptor");
+        return NXT_ERROR;
+    }
+
+    //TODO: Check code placement
+    //TODO: Check AK exit flow to free resources
+
+    signal(SIGINT, int_exit_shared);
+    signal(SIGTERM, int_exit_shared);
+    signal(SIGABRT, int_exit_shared);
+
+    return NXT_OK;
 }
 
-void
-nxt_application_kernel_new_port_handler(nxt_task_t *task, nxt_port_recv_msg_t *msg)
+static nxt_int_t
+nxt_application_kernel_greet_controller(nxt_task_t *task, nxt_port_t *controller_port)
+{
+    //TODO: Check code placement
+
+    nxt_int_t ret;
+
+    nxt_log(task, NXT_LOG_INFO, "Hello from application_kernel");
+
+    //run_socket_file_server(task);
+
+    ret = run_AF_XDP_umem_server(task);
+
+    if (nxt_slow_path(ret != NXT_OK))
+    {
+        return NXT_ERROR;
+    }
+
+    ret = nxt_port_socket_write(task, controller_port, NXT_PORT_MSG_PROCESS_READY,
+                                -1, 0, 0, NULL);
+    if (nxt_slow_path(ret != NXT_OK))
+    {
+        return NXT_ERROR;
+    }
+
+    return NXT_OK;
+}
+
+void nxt_application_kernel_new_port_handler(nxt_task_t *task, nxt_port_recv_msg_t *msg)
 {
     nxt_port_new_port_handler(task, msg);
 
-    if (msg->u.new_port != NULL
-        && msg->u.new_port->type == NXT_PROCESS_CONTROLLER)
+    if (msg->u.new_port != NULL && msg->u.new_port->type == NXT_PROCESS_CONTROLLER)
     {
         nxt_application_kernel_greet_controller(task, msg->u.new_port);
     }
 
-    if (msg->port_msg.stream == 0) {
+    if (msg->port_msg.stream == 0)
+    {
         return;
     }
 
-    if (msg->u.new_port == NULL
-        || msg->u.new_port->type != NXT_PROCESS_WORKER)
+    if (msg->u.new_port == NULL || msg->u.new_port->type != NXT_PROCESS_WORKER)
     {
         msg->port_msg.type = _NXT_PORT_MSG_RPC_ERROR;
     }
@@ -141,18 +243,19 @@ nxt_application_kernel_new_port_handler(nxt_task_t *task, nxt_port_recv_msg_t *m
 static void
 nxt_application_kernel_oosm_handler(nxt_task_t *task, nxt_port_recv_msg_t *msg)
 {
-    size_t                   mi;
-    uint32_t                 i;
-    nxt_bool_t               ack;
-    nxt_process_t            *process;
-    nxt_free_map_t           *m;
-    nxt_port_mmap_header_t   *hdr;
+    size_t mi;
+    uint32_t i;
+    nxt_bool_t ack;
+    nxt_process_t *process;
+    nxt_free_map_t *m;
+    nxt_port_mmap_header_t *hdr;
 
     nxt_debug(task, "oosm in %PI", msg->port_msg.pid);
 
     process = nxt_runtime_process_find(task->thread->runtime,
                                        msg->port_msg.pid);
-    if (nxt_slow_path(process == NULL)) {
+    if (nxt_slow_path(process == NULL))
+    {
         return;
     }
 
@@ -166,12 +269,15 @@ nxt_application_kernel_oosm_handler(nxt_task_t *task, nxt_port_recv_msg_t *msg)
 
     nxt_thread_mutex_lock(&process->incoming.mutex);
 
-    for (i = 0; i < process->incoming.size; i++) {
+    for (i = 0; i < process->incoming.size; i++)
+    {
         hdr = process->incoming.elts[i].mmap_handler->hdr;
         m = hdr->free_map;
 
-        for (mi = 0; mi < MAX_FREE_IDX; mi++) {
-            if (m[mi] != 0) {
+        for (mi = 0; mi < MAX_FREE_IDX; mi++)
+        {
+            if (m[mi] != 0)
+            {
                 ack = 1;
 
                 nxt_debug(task, "oosm: already free #%uD %uz = 0x%08xA",
@@ -184,19 +290,20 @@ nxt_application_kernel_oosm_handler(nxt_task_t *task, nxt_port_recv_msg_t *msg)
 
     nxt_thread_mutex_unlock(&process->incoming.mutex);
 
-    if (ack) {
-        (void) nxt_port_socket_write(task, msg->port, NXT_PORT_MSG_SHM_ACK,
-                                     -1, 0, 0, NULL);
+    if (ack)
+    {
+        (void)nxt_port_socket_write(task, msg->port, NXT_PORT_MSG_SHM_ACK,
+                                    -1, 0, 0, NULL);
     }
 }
 
 nxt_int_t
 nxt_application_kernel_start(nxt_task_t *task, void *data)
 {
-    nxt_int_t      ret;
-    nxt_port_t     *controller_port;
-    nxt_application_kernel_t   *application_kernel;
-    nxt_runtime_t  *rt;
+    nxt_int_t ret;
+    nxt_port_t *controller_port;
+    nxt_application_kernel_t *application_kernel;
+    nxt_runtime_t *rt;
 
     rt = task->thread->runtime;
 
@@ -204,25 +311,29 @@ nxt_application_kernel_start(nxt_task_t *task, void *data)
 
 #if (NXT_TLS)
     rt->tls = nxt_service_get(rt->services, "SSL/TLS", "OpenSSL");
-    if (nxt_slow_path(rt->tls == NULL)) {
+    if (nxt_slow_path(rt->tls == NULL))
+    {
         return NXT_ERROR;
     }
 
     ret = rt->tls->library_init(task);
-    if (nxt_slow_path(ret != NXT_OK)) {
+    if (nxt_slow_path(ret != NXT_OK))
+    {
         return ret;
     }
 #endif
 
     ret = nxt_http_init(task, rt);
-    if (nxt_slow_path(ret != NXT_OK)) {
+    if (nxt_slow_path(ret != NXT_OK))
+    {
         return ret;
     }
 
     //nxt_log(task, NXT_LOG_INFO, "In application kernel start fn2");
 
     application_kernel = nxt_zalloc(sizeof(nxt_application_kernel_t));
-    if (nxt_slow_path(application_kernel == NULL)) {
+    if (nxt_slow_path(application_kernel == NULL))
+    {
         return NXT_ERROR;
     }
 
@@ -237,8 +348,10 @@ nxt_application_kernel_start(nxt_task_t *task, void *data)
     //nxt_log(task, NXT_LOG_INFO, "In application kernel start fn4");
 
     controller_port = rt->port_by_type[NXT_PROCESS_CONTROLLER];
-    if (controller_port != NULL) {
-        nxt_application_kernel_greet_controller(task, controller_port);
+    if (controller_port != NULL)
+    {
+        ret = nxt_application_kernel_greet_controller(task, controller_port);
+        return ret;
     }
 
     //nxt_log(task, NXT_LOG_INFO, "In application kernel start fn5");
